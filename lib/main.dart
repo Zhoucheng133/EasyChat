4dd01500babed115f5628e2bbbf3efdb95399c1e
zhoucheng <zhoucheng133@outlook.com>
2025-02-05 19:43:55 +0800

    配置窗口最小尺寸

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ Future<void> main() async {
   await windowManager.ensureInitialized();
   WindowOptions windowOptions = const WindowOptions(
     size: Size(800, 600),
+    minimumSize: Size(800, 600),
     center: true,
     backgroundColor: Colors.transparent,
     skipTaskbar: false,
