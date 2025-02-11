dbe6d1c9a5aa966dcc28ac072be5268b7a3756db
zhoucheng <zhoucheng133@outlook.com>
2025-02-11 20:37:49 +0800

    添加设置的关于按钮

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,3 +1,4 @@
+import 'package:easy_chat/funcs/dialogs.dart';
 import 'package:flutter/material.dart';
 
 class Settings extends StatefulWidget {
@@ -8,10 +9,37 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+
+  final dialogs=Dialogs();
+
+  bool hoverAbout=false;
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("设置页"),
+    return Center(
+      child: GestureDetector(
+        onTap: ()=>dialogs.showAbout(context),
+        child: MouseRegion(
+          cursor: SystemMouseCursors.click,
+          onEnter: (_){
+            setState(() {
+              hoverAbout=true;
+            });
+          },
+          onExit: (_){
+            setState(() {
+              hoverAbout=false;
+            });
+          },
+          child: AnimatedDefaultTextStyle(
+            duration: const Duration(milliseconds: 200),
+            style: TextStyle(
+              color: hoverAbout ? Colors.amber[700] : Colors.black
+            ),
+            child: const Text("关于 EasyChat")
+          ),
+        ),
+      ),
     );
   }
 }
